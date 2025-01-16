84cb367187a4a23234fed393cb0ffdb7fb398e8b
HandsOnExp <danielohana6@gmail.com>
2025-01-16 16:02:05 +0200

    add command

--- Billing/src/app.py
+++ Billing/src/app.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
+#db connaction-------------------------------------------------------------------------------------
 db_host = os.getenv("DATABASE_HOST", "localhost")
 db_port = os.getenv("DATABASE_PORT", "3306")
 db_name = os.getenv("DATABASE_NAME", "mydatabase")
@@ -19,8 +20,10 @@ def get_db_connection():
         password=db_password
     )
 
+# add provider to tabel - post------------------------------------------------------------------------
 @app.route("/provider", methods=["POST"])
 def add_provider():
+    # get the name 
     data = request.get_json()
 
     if not data or "name" not in data:
@@ -29,32 +32,38 @@ def add_provider():
     provider_name = data["name"]
 
     try:
+        # open the connection
         conn = get_db_connection()
         cursor = conn.cursor()
 
+        # add provider to table
         query = "INSERT INTO Provider (name) VALUES (%s)"
         cursor.execute(query, (provider_name,))
         conn.commit()
 
-        provider_id = cursor.lastrowid  # קבלת ה-ID החדש
+        # get the id random
+        provider_id = cursor.lastrowid
 
+        # close connaction
         cursor.close()
         conn.close()
 
+        # return json new provider
         return jsonify({"id": provider_id, "name": provider_name}), 201
 
     except mysql.connector.Error as err:
         return jsonify({"error": str(err)}), 500
 
-    
+# home page--------------------------------------------------------------------------------------
 @app.route("/")
 def index():
     return "Welcome to the Flask App!"
 
-
+# chack health------------------------------------------------------------------------------------
 @app.route("/health")
 def health_check():
     return jsonify({"status": "OK"}), 200
 
+# main--------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
