521ed56a34aeda7d04cca5da8a37af04d4090ea0
TomerFeiler <tomermfeiler@gmail.com>
2025-01-20 12:21:42 +0200

    mailer and main changes

--- DevOps/main.py
+++ DevOps/main.py
@@ -3,56 +3,36 @@ from email.mime.text import MIMEText
 import smtplib
 import subprocess
 app = Flask(__name__)
-SMTP_SERVER = 'smtp.gmail.com' # e.g., smtp.gmail.com for Gmail
-SMTP_PORT = 587
-SMTP_USER = 'ganshmuelgreen1@gmail.com' # your email account for sending emails
-SMTP_PASSWORD = 'devopsgreen' # your email account password or app password
 
-def send_email(to_email, subject, body):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = SMTP_USER
-    msg['To'] = to_email
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(SMTP_USER, SMTP_PASSWORD)
-            server.sendmail(SMTP_USER, to_email, msg.as_string())
-            print(f"Email sent successfully to {to_email}")
-    except Exception as e:
-        print(f"Failed to send email: {str(e)}")
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
-    # Check if the event is for the 'main' branch (or any other branch you care about)
-    if data['ref'] == ['refs/heads/main', 'refs/heads/billing', 'refs/heads/weight']: # Only trigger for pushes to the main branch
-        # Extract the committer's email
-        pusher_email = data['pusher']['email']
-        commit_message = data['head_commit']['message']
-        # Send the email to the pusher (the person who pushed the changes)
-        send_email(
-            to_email=pusher_email,
-            subject="Your recent push to GitHub Repository",
-            body=f"Hi {data['pusher']['name']},\n\nYou recently pushed the following commit to the repository:\n\nCommit message: {commit_message}\n\nWe are testing your update!"
-)
-        return "Email sent", 200
-    else:
-        return "Not a push to tracked branches branch", 200
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+emails = ["tomermaas@gmail.com", "cl6724259@gmail.com", "mikoga89@gmail.com", "eitanp214@gmail.com", "tamirbuch@gmail.com",
+          "chencohen812@gmail.com", "Ester.Ovrani@gmail.com", "avivpeten123@gmail.com", "sara.beck.dev@gmail.com", "tomermfeiler@gmail.com"
+          ]
+
+gitUsers = ["tomermaas", "levi3259", "mikoga", "eitanp214", "tamirbu", "ChenCohen123", "EsterOvrani",
+            "aviv-peten", "sariGolombeck", "TomerFeiler"
+            ]
+
+mail_dict = dict(zip(gitUsers, emails))
+            
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     event = request.get_json()
     jsonBranch = event.get('ref') #'ref' is the JSON field containing the branch
+    jsonMail = event.get('sender', {}).get('login', 'Unknown user')
+    pusher_email = mail_dict.get(jsonMail, 'Unknown Email')
+    
+
+
     if jsonBranch and jsonBranch.startswith('refs/heads'):
         branchName = jsonBranch.split('/')[-1]
     else:
         return "Bad request: branch not found", 400
     try:
         app.logger.info(f"Building: {branchName}")
-        subprocess.run(['./builder.sh', branchName], check=True)
+        subprocess.run(['./builder.sh', branchName, pusher_email], check=True)
     except subprocess.CalledProcessError as e:
         app.logger.error(f"ERROR: {e}")
         return f'Server Error - cannot build for branch {branchName}', 500
