990868d9a6b4bdda265dcb8bbfdc0c99c971bd7c
miko <mikoga89@gmail.com>
2025-01-21 23:16:32 +0200

    prod start fixes

--- DevOps/prodStart.sh
+++ DevOps/prodStart.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-
-
 main_folder="/app"
 repo_folder="$main_folder/ganshmuelgreen"
 billing_folder="$repo_folder/Billing"
 weight_folder="$repo_folder/weight_team"
-
-
-
-
 
 
 git clone --single-branch --branch main https://github.com/tamirbu/ganshmuelgreen.git
@@ -18,7 +12,7 @@ if [ $? -ne 0 ]; then
     return 1
 fi
 docker-compose --env-file .env.prod build --no-cache
-docker-compose --env-file .env.prod up --build -d
+docker-compose --env-file .env.prod -p prod up --build -d
 cd -
 
 cd $billing_folder
@@ -27,12 +21,10 @@ if [ $? -ne 0 ]; then
     return 1
 fi
 docker-compose --env-file .env.prod build --no-cache
-docker-compose --env-file .env.prod up --build -d
+docker-compose --env-file .env.prod -p prod up --build -d
 cd -
 
-cd $main_folder
-echo "Removing $repo_folder..."
-rm -rf $repo_folder
+cd $main_folder && echo "Removing $repo_folder..." %%rm -rf $repo_folder
 
 
 
