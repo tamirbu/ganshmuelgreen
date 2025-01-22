0b66c7b47db221e35368eb20410ea51073db3de3
miko <mikoga89@gmail.com>
2025-01-22 11:04:14 +0200

    seperate projects for prod/test billing and weight

--- DevOps/builder.sh
+++ DevOps/builder.sh
@@ -26,7 +26,7 @@ docker_compose_build_n_up() {
         return 1
     fi
     docker-compose --env-file "$env_filename" build --no-cache
-    if [ $project -eq "test" ]; then
+    if [[ $project == test* ]]; then
         docker-compose --env-file "$env_filename" -p $project up --build -d --no-recreate
     else
         docker-compose --env-file "$env_filename" -p $project up --build -d
@@ -38,16 +38,16 @@ case "$branch_name" in
     "main")
         python3 mailer.py "Hello from the webserver" "$gitMail"
         git clone --single-branch --branch $branch_name https://github.com/tamirbu/ganshmuelgreen.git
-        docker_compose_build_n_up $weight_folder .env.test test
-        docker_compose_build_n_up $billing_folder .env.test test 
+        docker_compose_build_n_up $weight_folder .env.test test_weight
+        docker_compose_build_n_up $billing_folder .env.test test_billing 
         # Run E2E tests
-        cd $billing_folder && docker-compose --env-file .env.test -p test down
-        cd $weight_folder && docker-compose --env-file .env.test -p test down
+        cd $billing_folder && docker-compose --env-file .env.test -p test_billing down
+        cd $weight_folder && docker-compose --env-file .env.test -p test_weight down
         # if success:
          #mailer.py (message to send, gitMail)
-            cd $weight_folder && docker-compose --env-file .env.prod -p prod down && \
+            cd $weight_folder && docker-compose --env-file .env.prod -p prod_weight down && \
             docker_compose_build_n_up $weight_folder .env.prod prod
-            cd $billing_folder && docker-compose --env-file .env.prod -p prod down && \
+            cd $billing_folder && docker-compose --env-file .env.prod -p prod_billing down && \
             docker_compose_build_n_up $billing_folder .env.prod prod
         # else:
         #mailer.py (message to send, gitMail)
@@ -56,25 +56,26 @@ case "$branch_name" in
     "devops")
         echo 'devops is not automatically built and deployed'
         python3 mailer.py "Hello, you pushed into Devops branch", "$gitMail"
+        exit 0
         ;;
     "billing"|"weight")
         git clone --single-branch --branch $branch_name https://github.com/tamirbu/ganshmuelgreen.git
-        docker_compose_build_n_up $weight_folder .env.test test
-        docker_compose_build_n_up $billing_folder .env.test test 
+        docker_compose_build_n_up $weight_folder .env.test test_weight
+        docker_compose_build_n_up $billing_folder .env.test test_billing 
         # Run E2E tests
         # if success:
         python3 mailer.py "Hello, you pushed into a branch on git", "$gitMail"
         # else:
         #     send failure success to push
-        cd $billing_folder && docker-compose --env-file .env.test -p test down
-        cd $weight_folder && docker-compose --env-file .env.test -p test down
+        cd $billing_folder && docker-compose --env-file .env.test -p test_billing down
+        cd $weight_folder && docker-compose --env-file .env.test -p test_weight down
         ;;
     *)
         echo "Unknown branch name: $branch_name"
-        exit 1
+        exit 0
         ;;
 esac
-cd $main_folder
-echo "removing $repo_folder..."
-rm -rf $repo_folder
+cd $main_folder && \
+echo "removing $repo_folder..." && \
+rm -rf $repo_folder && \
 echo "Script executed successfully!"
